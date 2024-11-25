d72dddff786d0fd2683dfed749e6ca7abbce4f75
Puneet Shrivastav <90194089+wdAres@users.noreply.github.com>
2024-11-25 10:16:13 +0530

    Update Notice.jsx

--- src/components/noticeboard/Notice.jsx
+++ src/components/noticeboard/Notice.jsx
@@ -7,7 +7,7 @@ const Notice = ({ label, createdAt, file , url , latest=false }) => {
   return (
     <div className={classes.notice}>
       <FaRegHandPointRight className={classes.notice_icon} />
-      <a target='_blank' href={url?url:'http://127.0.0.1:8001/'+file} className={classes.notice_p}>{moment(createdAt).format('DD-MM-YYYY')} - {label} {latest && <span className={classes.notice_span}>latest</span>}</a>
+      <a target='_blank' href={url?url:`http://127.0.0.1:8001/${file}`} className={classes.notice_p}>{moment(createdAt).format('DD-MM-YYYY')} - {label} {latest && <span className={classes.notice_span}>latest</span>}</a>
 
     </div>
   )
