d572421fc5e349131dddc7ddf95b01d5e3b31dd8
Puneet Shrivastav <90194089+wdAres@users.noreply.github.com>
2024-11-29 14:42:04 +0530

    update

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,6 +1,5 @@
 const url = import.meta.env.VITE_API_URL ;
 
-// dafadg ff
 // export const BASE_URL = 'http://127.0.0.1:8001/api/v1/school'
 export const BASE_API = url;
 export const BASE_URL = `${url}/api/v1/website`
