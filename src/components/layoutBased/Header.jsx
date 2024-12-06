c1c2e0ff0db06993dcaf65b8daf005c47806ce5f
Abhilekh Singh <pravinkumarmishra432@gmail.com>
2024-12-06 19:17:31 +0530

    update

--- src/components/layoutBased/Header.jsx
+++ src/components/layoutBased/Header.jsx
@@ -4,51 +4,67 @@ import { RxHamburgerMenu } from 'react-icons/rx'
 import { NavLink } from 'react-router-dom'
 import GoogleTranslate from '../GoogleTranslate'
 
-const Header = ({openSidebar}) => {
+const Header = ({ openSidebar }) => {
   return (
     <header className={classes.header}>
-        <div className={`${classes.inner_header} universal_width`}>
+      <div className={`${classes.inner_header} universal_width`}>
         <a className={classes.header_logo} href="#!">
           <img src="/images/bssp_logo.png" alt="" />
           <div className={classes.header_logo_inner_div}>
             <h4>Bihar Sanskrit</h4>
             <h6>Education Board Patna</h6>
           </div>
-          </a>
+        </a>
         <nav className={classes.header_nav}>
-            <NavLink to={'/'} className={classes.header_nav_link}>Home</NavLink>
-            <NavLink to={'/courses'} className={classes.header_nav_link}>Academic</NavLink>
-            {/* <NavLink to={'/about'} className={classes.header_nav_link}>About</NavLink> */}
-            <div className={`${classes.header_nav_link} ${classes.db_links}`}>
-            About
-           <div className={classes.hidden_links}>
-           <NavLink to={'/about/general_info'} className={classes.header_nav_link}>General Information</NavLink>
-                    <NavLink to={'/about/chairman_message'} className={classes.header_nav_link}>Chairman's Message</NavLink>
-                    <NavLink to={'/about/history'} className={classes.header_nav_link}>History</NavLink>
-                    <NavLink to={'/about/rules'} className={classes.header_nav_link}>Rules & Regulations</NavLink>
-                    {/* <NavLink to={'/about/objectives'} className={classes.header_nav_link}>Objectives</NavLink> */}
-                    <NavLink to={'/about/aim'} className={classes.header_nav_link}>Aim</NavLink>
-           </div>
-           </div>
-            <NavLink className={classes.header_nav_link} to="/admissions">Admissons</NavLink>
-            <NavLink className={classes.header_nav_link} to="/authorised_centers">Authorised Centers</NavLink>
-            <NavLink className={classes.header_nav_link} to="/results">Results</NavLink>
-            <NavLink className={classes.header_nav_link} to="/contact">Contact Us</NavLink>
-           <div className={`${classes.header_nav_link} ${classes.db_links}`}>
-           Dashboard Links 
-           <div className={classes.hidden_links}>
-           <a target='_blank' href='http://13.127.79.175:8082/login' className={classes.header_nav_link} to="/contact">School Login</a>
-           <a target='_blank' href='http://13.127.79.175:8083/login' className={classes.header_nav_link} to="/contact">Board Login</a>
-           </div>
-           </div>
+          <NavLink to={'/'} className={classes.header_nav_link}>Home</NavLink>
+
+          {/* <NavLink to={'/about'} className={classes.header_nav_link}>About</NavLink> */}
+          <div className={`${classes.header_nav_link} ${classes.db_links}`}>
+            ABOUT BSSB
+            <div className={classes.hidden_links}>
+              <NavLink to={'/about/general_info'} className={classes.header_nav_link}>ABOUT US</NavLink>
+              <NavLink to={'/about/chairman_message'} className={classes.header_nav_link}>CHAIRMAN’S MESSAGE</NavLink>
+              <NavLink to={'/about/chairman_message'} className={classes.header_nav_link}>COBSE</NavLink>
+              {/* <NavLink to={'/about/general_info'} className={classes.header_nav_link}>General Information</NavLink> */}
+              
+              <NavLink to={'/about/history'} className={classes.header_nav_link}>HISTORY</NavLink>
+              <NavLink to={'/about/aim'} className={classes.header_nav_link}>AIM</NavLink>
+              <NavLink to={'/about/rules'} className={classes.header_nav_link}>RULE AND REGULATIONS</NavLink>
+              {/* <NavLink to={'/about/objectives'} className={classes.header_nav_link}>Objectives</NavLink> */}
+              
+            </div>
+          </div>
+          <div className={`${classes.header_nav_link} ${classes.db_links}`}>
+          Academic
+            <div className={classes.hidden_links}>
+              <NavLink to={'/courses/general_info'} className={classes.header_nav_link}>TYPE OF SCHOOL</NavLink>
+              <NavLink to={'/courses/chairman_message'} className={classes.header_nav_link}>STUDENTS</NavLink>
+              <NavLink to={'/courses/chairman_message'} className={classes.header_nav_link}>SYLLABUS</NavLink>              
+              <NavLink to={'/courses/history'} className={classes.header_nav_link}>FEES STRUCTURE</NavLink>
+              
+            </div>
+          </div>
+          
+          {/* <NavLink className={classes.header_nav_link} to="/admissions">Admissons</NavLink> */}
+          {/* <NavLink className={classes.header_nav_link} to="/authorised_centers">Authorised Centers</NavLink> */}
+          {/* <NavLink className={classes.header_nav_link} to="/results">Results</NavLink> */}
+          <NavLink className={classes.header_nav_link} to="/contact">Contact Us</NavLink>
+          <NavLink to={'/gallary'} className={classes.header_nav_link}>GALLARY</NavLink>
+          <div className={`${classes.header_nav_link} ${classes.db_links}`}>
+            Dashboard Links
+            <div className={classes.hidden_links}>
+              <a target='_blank' href='http://13.127.79.175:8082/login' className={classes.header_nav_link} to="/contact">School Login</a>
+              <a target='_blank' href='http://13.127.79.175:8083/login' className={classes.header_nav_link} to="/contact">Board Login</a>
+            </div>
+          </div>
         </nav>
         <button className={classes.ham_burger_menu}>
-            <RxHamburgerMenu color='white' onClick={openSidebar} size={20} />
+          <RxHamburgerMenu color='white' onClick={openSidebar} size={20} />
         </button>
         <div className={classes.pos_ab}>
-        <GoogleTranslate />
+          <GoogleTranslate />
         </div>
-        </div>
+      </div>
     </header>
   )
 }
